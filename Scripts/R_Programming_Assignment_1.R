d494050b351f39b5509c6d51bd2782c6cb40f866
Matt Denny <matthewjdenny@gmail.com>
2017-05-18 13:35:26 -0400

    start of first assignment

---
+++ Scripts/R_Programming_Assignment_1.R
@@ -0,0 +1,11 @@
+###### R Programming: Assignment 1 #####
+
+
+### Preliminaries ###
+rm(list = ls())
+
+# Set your working directory to the workshop repo location:
+setwd("~/Documents/RA_and_Consulting_Work/ISSR_Data_Management_Web_Scraping_2017/Data")
+
+# Load in the data we will be using:
+load("Example_Tweets.RData")
